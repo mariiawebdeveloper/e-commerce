6531829c49c95eafa49fdeff68a513020de48c2a
MariiaMarchenko <96719405+MariiaMarchenko@users.noreply.github.com>
2023-05-06 23:39:29 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Navbar} from "./components/navbar";
+import {Shop} from "./pages/shop/shop";
+import {Contact} from "./pages/contact";
+import {Cart} from "./pages/cart/cart";
+import {ProductDetails} from "./pages/productDetails/productDetails";
+import {ShopContextProvider} from "./context/shop-context";
+import {Footer} from "./components/footer";
+import Aboutus from "./pages/aboutUs/aboutus";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ShopContextProvider>
+                <Router>
+                    <Navbar/>
+                    <Routes>
+                        <Route path="/" element={<Shop/>}/>
+                        <Route path="/contact" element={<Contact/>}/>
+                        <Route path="/cart" element={<Cart/>}/>
+                        <Route path="/aboutus" element={<Aboutus/>}/>
+                        <Route path="/product/:id" element={<ProductDetails/>}/>
+                    </Routes>
+                    <Footer/>
+                </Router>
+            </ShopContextProvider>
+        </div>
+    );
 }
 
 export default App;
